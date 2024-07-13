a6725152ab817f984a6a3a1707f2e29c791cf9ec
Amsterdan Vasconcelos <amsterdan.sci@gmail.com>
2024-07-13 05:41:59 -0300

    feat: install dotenv, create .env and .env.example files, and create ts.config fie

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
 import { server } from './server/server';
 
-server.listen(3333, () => console.log('App rodando na porta 3333'));
+const port = process.env.PORT || 3333;
+
+server.listen(port, () => console.log(`App rodando na porta ${port}`));
