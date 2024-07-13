8848729d89e14602b11832bac2dfafe0c1dd1c81
Amsterdan Vasconcelos <amsterdan.sci@gmail.com>
2024-07-13 19:27:55 -0300

    feat: install library yup for form validation. Add yup in the create router file.

--- src/server/controllers/citys/create.ts
+++ src/server/controllers/citys/create.ts
@@ -1,11 +1,30 @@
 import { Request, Response } from 'express';
+import * as yup from 'yup';
 
 type city = {
   name: string;
 };
 
-const create = (req: Request<{}, {}, city>, res: Response) => {
-  return res.json(`Criada a cidade de ${req.body.name}`);
+const cityValidation: yup.Schema<city> = yup.object().shape({
+  name: yup.string().required().min(3),
+});
+
+const create = async (req: Request<{}, {}, city>, res: Response) => {
+  let validatedData: city | undefined = undefined;
+
+  try {
+    validatedData = await cityValidation.validate(req.body);
+  } catch (error) {
+    const yupError = error as yup.ValidationError;
+
+    return res.json({
+      errors: {
+        default: yupError.message,
+      },
+    });
+  }
+
+  return res.json(validatedData);
 };
 
 export { create };
