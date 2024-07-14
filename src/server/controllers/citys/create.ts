37bde56e8c597ad6046af5bca37da750d1b73f25
Amsterdan Vasconcelos <amsterdan.sci@gmail.com>
2024-07-14 09:50:42 -0300

    feat: create erros in create.ts router

--- src/server/controllers/citys/create.ts
+++ src/server/controllers/citys/create.ts
@@ -1,27 +1,35 @@
 import { Request, Response } from 'express';
+import { StatusCodes } from 'http-status-codes';
 import * as yup from 'yup';
 
 type city = {
   name: string;
+  state: string;
 };
 
 const cityValidation: yup.Schema<city> = yup.object().shape({
   name: yup.string().required().min(3),
+  state: yup.string().required().min(3),
 });
 
 const create = async (req: Request<{}, {}, city>, res: Response) => {
   let validatedData: city | undefined = undefined;
 
   try {
-    validatedData = await cityValidation.validate(req.body);
-  } catch (error) {
-    const yupError = error as yup.ValidationError;
-
-    return res.json({
-      errors: {
-        default: yupError.message,
-      },
+    validatedData = await cityValidation.validate(req.body, {
+      abortEarly: false,
     });
+  } catch (err) {
+    const yupError = err as yup.ValidationError;
+    const errors: Record<string, string> = {};
+
+    yupError.inner.forEach((error) => {
+      if (!error.path) return;
+
+      errors[error.path] = error.message;
+    });
+
+    return res.status(StatusCodes.BAD_REQUEST).json({ errors });
   }
 
   return res.json(validatedData);
