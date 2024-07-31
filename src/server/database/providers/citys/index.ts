a75621a80e1fb1168d83f120ec82eaf22eb9bafa
Amsterdan Vasconcelos <amsterdan.sci@gmail.com>
2024-07-30 23:16:24 -0300

    feat: add news function to export citiesProvider

--- src/server/database/providers/citys/index.ts
+++ src/server/database/providers/citys/index.ts
@@ -1,7 +1,17 @@
 import * as create from './create';
+import * as getAll from './getAll';
+import * as getById from './getById';
+import * as updateById from './updateById';
+import * as deleteById from './deleteById';
+import * as count from './count';
 
 const citiesProviders = {
   ...create,
+  ...getAll,
+  ...getById,
+  ...updateById,
+  ...deleteById,
+  ...count,
 };
 
 export { citiesProviders };
