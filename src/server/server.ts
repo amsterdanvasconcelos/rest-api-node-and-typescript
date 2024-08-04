50e77c8922e4c6e354d570807cfbbd40fb626da7
Amsterdan Vasconcelos <amsterdan.sci@gmail.com>
2024-08-04 17:22:49 -0300

    feat: add users.router

--- src/server/server.ts
+++ src/server/server.ts
@@ -1,11 +1,12 @@
 import 'dotenv/config';
 import express from 'express';
-import { cities, people } from './routes';
+import { cities, people, users } from './routes';
 
 const server = express();
 
 server.use(express.json());
 server.use(cities.router);
 server.use(people.router);
+server.use(users.router);
 
 export { server };
