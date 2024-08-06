b837e08d78fed189e874041183a2ba5f7f1ac0e8
Amsterdan Vasconcelos <amsterdan.sci@gmail.com>
2024-08-06 18:16:26 -0300

    integrate jwtService with ensureAuthenticated

--- src/server/shared/middlewares/ensureAuthenticated.ts
+++ src/server/shared/middlewares/ensureAuthenticated.ts
@@ -1,6 +1,7 @@
 import { RequestHandler } from 'express';
 import { StatusCodes } from 'http-status-codes';
 import { getJsonError } from '../../controllers/getJsonError';
+import { jwtService } from '../services';
 
 const ensureAuthenticated: RequestHandler = async (req, res, next) => {
   const { authorization } = req.headers;
@@ -12,14 +13,29 @@ const ensureAuthenticated: RequestHandler = async (req, res, next) => {
   }
 
   const [type, token] = authorization.split(' ');
-  const isValideType = type === 'Bearer';
-  const isValideToken = token === 'test.test.test';
 
-  if (!isValideType || !isValideToken) {
+  const isValideType = type === 'Bearer';
+  if (!isValideType) {
     return res
       .status(StatusCodes.UNAUTHORIZED)
       .json(getJsonError('Não autenticado!'));
   }
+
+  const jwtData = jwtService.verify(token);
+
+  if (jwtData === 'JWT_SECRET_NOT_FOUND') {
+    return res
+      .status(StatusCodes.INTERNAL_SERVER_ERROR)
+      .json(getJsonError('Erro ao verificar o token!'));
+  }
+
+  if (jwtData === 'INVALID_TOKEN') {
+    return res
+      .status(StatusCodes.UNAUTHORIZED)
+      .json(getJsonError('Não autenticado!'));
+  }
+
+  req.headers.uid = jwtData.uid.toString();
 
   return next();
 };
